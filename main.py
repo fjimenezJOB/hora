06b40873443c9131165283b33f304188cdd66858
fjimenezJOB <fjimenezjob@gmail.com>
2019-09-23 15:44:28 +0200

    proyecto feo acabado, solo faltaba sacar el regitro en pantalla...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# IMPORTS DE LAS LIBRERIAS DE FLASK
 from flask import Flask, render_template, redirect, url_for, request
+# imports de otras librerias
 import csv
 import json
 
@@ -9,26 +11,34 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# Metodo para escribir en archivo.csv, la informacion llega a traves de una cookie que se genera en el navegador despues de darle a enviar.
+def escribir(fecha='00/00/0000', dinero=0, horas=0, lugar=' '):
 
-def escribir(methods=['POST']):
+    info = json.loads(request.cookies.get('data'))#coje la informacion del json de la cookie.
 
-    data = json.loads(request.cookies.get('data'))
-
-    fecha = data.get('fecha')
-    dinero = data.get('dinero')
-    horas = data.get('horas')
-    lugar = data.get('lugar')
+    fecha = info.get('fecha')
+    dinero = info.get('dinero')
+    horas = info.get('horas')
+    lugar = info.get('lugar')
 
     with open('registro.csv', 'a') as f:
-        f.write(f'{fecha},{dinero},{horas},{lugar}\n')
+        f.write(f'{fecha},{dinero},{horas},{lugar}\n')#escribe la informacion en el archivo csv.
         f.close()
 
-
+# metodo para leer el archivo registro.csv (inacabada)
 @app.route('/registro')
-def registro(fecha='00/00/00', dinero=0, horas=0, lugar=""):
-    return render_template('registro.html')
+def registro():
+    registros = {}
+    i = 0
+    with open('registro.csv', 'r') as f:
+        for row in f:
+            registros[f'linea{i}'] = row
+            i+=1
+        
 
+    return render_template('registro.html',**registros)
 
+# Metodo que genera la cookie y llama al metodo de escribir() para que se escriba en el archivo.
 @app.route('/enviar', methods=['POST'])
 def enviar():
     response = redirect(url_for('home'))
