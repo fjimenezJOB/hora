228f8eeab5f8ff38c20c6dfb39ccecf4329692aa
fjimenezJOB <fjimenezjob@gmail.com>
2019-10-04 20:23:54 +0200

    Commit de seguridad

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 # imports de otras librerias
 import csv
-import json
 
 app = Flask(__name__)
 
@@ -12,38 +11,36 @@ def home():
     return render_template('index.html')
 
 # Metodo para escribir en archivo.csv, la informacion llega a traves de una cookie que se genera en el navegador despues de darle a enviar.
+
+
 def escribir(fecha='00/00/0000', dinero=0, horas=0, lugar=' '):
 
-    info = json.loads(request.cookies.get('data'))#coje la informacion del json de la cookie.
-
-    fecha = info.get('fecha')
-    dinero = info.get('dinero')
-    horas = info.get('horas')
-    lugar = info.get('lugar')
-
     with open('registro.csv', 'a') as f:
-        f.write(f'{fecha},{dinero},{horas},{lugar}\n')#escribe la informacion en el archivo csv.
+        # escribe la informacion en el archivo csv.
+        f.write(f'{fecha},{dinero},{horas},{lugar}\n')
         f.close()
 
 # metodo para leer el archivo registro.csv (inacabada)
 @app.route('/registro')
 def registro():
-    registros = {}
-    i = 0
-    with open('registro.csv', 'r') as f:
-        for row in f:
-            registros[f'linea{i}'] = row
-            i+=1
-        
 
-    return render_template('registro.html',**registros)
+    resultados = []
+    with open("registro.csv") as archivo:
+        reader = csv.reader(archivo)
+        for row in reader:
+            resultados.append(row)
+
+    return render_template('registro.html', **resultados)
 
 # Metodo que genera la cookie y llama al metodo de escribir() para que se escriba en el archivo.
 @app.route('/enviar', methods=['POST'])
 def enviar():
+    fecha = request.form.get('fecha')
+    lugar = request.form.get('lugar')
+    dinero = request.form.get('dinero')
+    horas = request.form.get('horas')
     response = redirect(url_for('home'))
-    response.set_cookie('data', json.dumps(dict(request.form.items())))
-    escribir()
+    escribir(fecha, dinero, horas, lugar)
     return response
 
 
@@ -53,4 +50,4 @@ def error(error):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', '5000', debug=True)
+    app.run('0.0.0.0', '8080', debug=True)
